d254cb34b4959471d8d078fc8f74389b5cb3c5df
winkerVSbecks <varunvachhar@gmail.com>
2024-10-17 12:27:40 -0400

    add test for add to cart flow

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,13 +1,15 @@
-import { test, expect } from '@chromatic-com/playwright'
+import { test, expect, takeSnapshot } from '@chromatic-com/playwright'
 
-test.skip('Order created', async ({ page }, testInfo) => {
+test('Order created', async ({ page }, testInfo) => {
   await page.goto('/restaurants/2')
 
   await page.waitForTimeout(1000)
+  await takeSnapshot(page, testInfo)
 
   await page.getByText('Fries').first().click()
 
   await expect(page.getByTestId('modal')).toBeVisible()
+  await takeSnapshot(page, testInfo)
 
   await page.getByText(/add for €2\.50/).click()
 

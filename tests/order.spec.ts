2b80f5f96524fa5ad7f41c85ff04fddb99963c3e
winkerVSbecks <varunvachhar@gmail.com>
2024-10-16 21:56:42 -0400

    clean up

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,15 +1,13 @@
-import { test, expect, takeSnapshot } from '@chromatic-com/playwright'
+import { test, expect } from '@chromatic-com/playwright'
 
-test('Order created', async ({ page }, testInfo) => {
+test.skip('Order created', async ({ page }, testInfo) => {
   await page.goto('/restaurants/2')
 
   await page.waitForTimeout(1000)
-  await takeSnapshot(page, testInfo)
 
   await page.getByText('Fries').first().click()
 
   await expect(page.getByTestId('modal')).toBeVisible()
-  await takeSnapshot(page, testInfo)
 
   await page.getByText(/add for €2\.50/).click()
 
